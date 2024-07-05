0d996700ef07d4bee1956b4f11e29623ef3bfe3e
nomadsb <162993426+nomadsb@users.noreply.github.com>
2024-07-05 09:51:41 +0200

    Update test_schema.py

--- tests/schema_packages/test_schema.py
+++ tests/schema_packages/test_schema.py
@@ -4,8 +4,8 @@ from nomad.client import normalize_all, parse
 
 
 def test_schema():
-    test_file = os.path.join('tests', 'data', 'test.archive.yaml')
+    test_file = os.path.join('tests', 'data', 'semtest.archive.yaml')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    assert entry_archive.data.message == 'Hello Sabine!'
